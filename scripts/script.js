1e759556420c5f6565192d1cd0c14be185d0be5e
jared <jared.jacobson.2000@gmail.com>
2017-11-25 22:37:58 -0700

    styled the course select, and got it to populate

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,13 @@ let localObj = {latitude:40.4426135, longitude:-111.8631116, radius: 100};
 $(document).ready(function () {
     $.post("https://golf-courses-api.herokuapp.com/courses", localObj, function (data, status) {
         closeCourses = JSON.parse(data);
-        for(let p in closeCourses) {
-            console.log(closeCourses);
-        }
+        let courses = closeCourses.courses;
+        courses.forEach(function (value) {
+            addCourse(value);
+        });
     });
 });
+function addCourse(course) {
+    let element = '<option value="' + course.id + '">' + course.name + '</option>';
+    $('#courseSelect').append(element);
+}
