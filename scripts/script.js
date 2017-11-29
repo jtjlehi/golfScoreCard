531258a2805cbc4c9c19dd726142f0a17956f057
jared <jared.jacobson.2000@gmail.com>
2017-11-28 19:51:09 -0700

    set up session storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,17 @@
-//let closeCourses;
-let localObj = {latitude:40.4426135, longitude:-111.8631116, radius: 100};
 $(document).ready(function () {
-    //grab the courses to choose from
-    $('#zipCode').keyup(function (event) {
-         if(event.which === 13) {
-            getLocation();
-        }
-    });
-});
-function addCourse(course) {
-    let element = '<option value="' + course.id + '" id="'+ course.id + '">' + course.name + '</option>';
-    $('#courseSelect').append(element);
-}
-
-function loadCourses(locationInfo) {
-    $.post("https://golf-courses-api.herokuapp.com/courses", locationInfo, function (data, status) {
-        let allCourses = JSON.parse(data);
-        let courses = allCourses.courses;
-        courses.forEach(function (value) {
-            addCourse(value);
+    let courseInfo = sessionStorage.getItem('courseInfo');
+    if(courseInfo === undefined) {
+        //grab the courses to choose from
+        $('#zipCode').keyup(function (event) {
+            if (event.which === 13) {
+                getLocation();
+            }
         });
-        $('.courseSelectCont').removeClass('hidden');
-        $('.locationSelectCont').addClass('hidden');
-    });
-    $('#startButton').on('click', function () {
-        let courseId = $('#courseSelect').val();
-        selectCourse(courseId);
-    });
-}
-//request for the data to load the game card
-function selectCourse(courseId) {
-    $.ajax('https://golf-courses-api.herokuapp.com/courses/' + courseId, {
-        success: function (data) {
-            startGame(JSON.parse(data));
-        }
-    });
-}
+    }
+    else{
+        startGame(JSON.parse(courseInfo));
+    }
+});
 //pull lat and long based on zip code
 function getLocation(func) {
     let zipCode = $('#zipCode').val();
@@ -51,7 +27,39 @@ function getLocation(func) {
         }
     });
 }
+function loadCourses(locationInfo) {
+    $.post("https://golf-courses-api.herokuapp.com/courses", locationInfo, function (data, status) {
+        let allCourses = JSON.parse(data);
+        let courses = allCourses.courses;
+        courses.forEach(function (value) {
+            //add each course into the start screen selection thing
+            addCourse(value);
+        });
+        $('.courseSelectCont').removeClass('hidden');
+        $('.locationSelectCont').addClass('hidden');
+    });
+    $('#startButton').on('click', function () {
+        let courseId = $('#courseSelect').val();
+        //actually select the course and start the game.
+        selectCourse(courseId);
+    });
+}
+//the function to add courses
+function addCourse(course) {
+    let element = '<option value="' + course.id + '" id="'+ course.id + '">' + course.name + '</option>';
+    $('#courseSelect').append(element);
+}
+//request for the data to load the game card
+function selectCourse(courseId) {
+    $.ajax('https://golf-courses-api.herokuapp.com/courses/' + courseId, {
+        success: function (data) {
+            let courseInfo = JSON.parse(data);
+            sessionStorage.setItem("courseInfo", JSON.stringify(courseInfo));
+            startGame(courseInfo);
+        }
+    });
+}
 //actually loading the game card
 function startGame(courseInfo) {
-    console.log(courseInfo);
+    $('')
 }
