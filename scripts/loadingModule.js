db669867ff1d11281d8a3cfecd2c263afa0b38a0
jared <jared.jacobson.2000@gmail.com>
2017-12-05 19:16:14 -0700

    removed some testing console.log()'s

--- scripts/loadingModule.js
+++ scripts/loadingModule.js
@@ -1,11 +1,9 @@
 export function displayLoadScreen() {
     $('.loadCont').removeClass('hidden');
-    console.log('display fired')
 }
 export function hideLoadScreen() {
     setTimeout(() => {
 
     }, 300);
     $('.loadCont').addClass('hidden');
-    console.log('remove fired')
 }
